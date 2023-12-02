079c350d9db933b689daf6b7a0b60b100e62a869
Abdelrahman Elsayed Mohamed <60861577+abdelrahman99999@users.noreply.github.com>
2023-12-02 19:29:56 +0200

    utilities used for silent bootloader updater

--- Utilities/binary_to_array.py
+++ Utilities/binary_to_array.py
@@ -3,7 +3,7 @@ def convert_binary_to_c_array(input_file, output_file, array_name):
         binary_data = file.read()
 
     with open(output_file, 'w') as file:
-        file.write(f"unsigned char {array_name}[] = {{\n")
+        file.write(f"uint8_t {array_name}[] = {{\n")
         
         for byte in binary_data:
             file.write(f"0x{byte:02x}, ")
@@ -11,7 +11,7 @@ def convert_binary_to_c_array(input_file, output_file, array_name):
         file.write("\n};\n")
 
 
-input_file = 'bootloader.bin'
+input_file = 'bootloader_v345.bin'
 output_file = 'output.c'
 array_name = 'bootloader_as_array'
 convert_binary_to_c_array(input_file, output_file, array_name)
